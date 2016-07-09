8b28ba905fc644911f32fe015c2b6f6e7c658e7a
aHooHa <HY_HooH@aliyun.com>
2016-07-09 23:52:16 +0800

    first day

---
+++ hello/src/com/sunyahui/action/HelloWorldAction.java
@@ -0,0 +1,19 @@
+package com.sunyahui.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction  extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String execute() throws Exception {
+		System.out.println("执行execute方法");
+		return SUCCESS;
+	}
+
+	
+}
